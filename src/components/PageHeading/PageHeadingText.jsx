31a3508a9072c2ce97f3ee6437ca0ceae059ad49
isaias garcia <isaico@outlook.com>
2022-02-23 15:25:08 -0300

    seccion de contacto en desarrollo

--- src/components/PageHeading/PageHeadingText.jsx
+++ src/components/PageHeading/PageHeadingText.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PageHeadingText = (props) => {
   return (
     <>
-      <h3 className="mx-3 text-2xl font-semibold text-gray-600"> {props.text} </h3>
+      <h3 className="mx-3 text-2xl font-Poppins font-semibold text-gray-600"> {props.text} </h3>
     </>
   );
 };
--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -1,17 +1,72 @@
 import React from "react";
-import { FaTelegramPlane } from "react-icons/fa"; 
+import {
+  FaTelegramPlane,
+  FaWhatsapp,
+  FaInstagram,
+  FaFacebook,
+} from "react-icons/fa";
+import {GoLocation} from "react-icons/go"
 import PageHeading from "../components/PageHeading/PageHeading";
 import Mapa from "../components/PageHeading/Mapa";
+
 const Contact = () => {
   return (
     <div className="max-w-7xl  mx-auto py-16 sm:py-24 lg:py-32 ">
-      <div className="flex justify-center items-center my-5">
-        <PageHeading text="Contact" icon={FaTelegramPlane}/>
-      </div>
-      <div className="flex">
+      <div className="max-w-[50%] mx-auto">
+        <div className="flex justify-center items-center my-5">
+          <PageHeading text="Contacto" icon={FaTelegramPlane} />
+        </div>
         <div>
-          <h3>Ubicacion</h3>
-          <Mapa/>
+          <h3 className="text-xl my-10  font-semibold text-gray-600">
+            {" "}
+            Si tenes dudas, no esperes mas y escribinos en cualquiera de
+            nuestras redes! estamos disponibles 24/7 para asesorarte!
+          </h3>
+          <div className="flex justify-between mx-auto">
+            <div className="contact-btn">
+              <a href="#">
+                {" "}
+                <FaFacebook className="md:w-10 md:h-10 " />
+              </a>
+            </div>
+            <div className="contact-btn">
+              <a href="#">
+                <FaInstagram className="md:w-10 md:h-10" />
+              </a>
+            </div>
+            <div className="contact-btn">
+              <a href="#">
+                {" "}
+                <FaWhatsapp className="md:w-10 md:h-10" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col w-full md:flex-wrap my-5 ">
+          <h3 className="text-xl  text-gray-800 font-medium">
+            Informacion
+          </h3>
+          <div className="flex flex-col w-full my-10" id="contact-info">
+            <div className="contact-col bg-slate-300">
+              <div className="contact-col-title">Dirección</div>
+              <div className="contact-col-title">Calle asdasd423</div>
+            </div>
+            <div className="contact-col bg-slate-100">
+              <div className="contact-col-title">Teléfono</div>
+              <div className="contact-col-title">223-1111111</div>
+            </div>
+            <div className="contact-col bg-slate-300">
+              <div className="contact-col-title">E-mail</div>
+              <div className="contact-col-title">asdasd@asdasd.com</div>
+            </div>
+            {/*  */}
+          </div>
+          <div className="drop-shadow-lg">
+              <h3 className=" text-xl mb-5 text-gray-800 font-medium">
+                Ubicacion
+              </h3>
+              <Mapa />
+            </div>
         </div>
       </div>
     </div>
